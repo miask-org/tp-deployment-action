53b7b555e368064f7972da79c525902a9bb3c8d7
mastermind008 <link2muzammal@gmail.com>
2021-04-19 06:43:33 +0500

    Added get envId api call

--- src/action.js
+++ src/action.js
@@ -3,31 +3,30 @@ const github = require('@actions/github');
 const axios = require('axios');
 const FormData = require('form-data');
 
-let deployArgs = {};
-
 async function main() {
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  deployArgs = parseJSON(core.getInput('deployArgs'));
-  console.log(deployArgs);
+  const GITHUB_TOKEN = process.env.github_token;
+  const CLIENT_ID = process.env.client_id;
+  const CLIENT_SECRET = process.env.client_secret;
+  const ORG_ID = 'fdebe0d5-a2d7-4594-b1d3-db10e283e63b';
 
-  if (!deployArgs) return;
+  const release_tag = core.getInput('release-tag');
+  const cloudhub_apps = parseJSON(core.getInput('cloudhub-apps'));
 
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
 
   try {
-    const release = await getRelease(octokit, context);
-    const {id, name} = filterArtifact(release);
-    console.log(`artifact_id: ${id},  artifact_name: ${name}`);
+    const release = await getRelease(octokit, context, release_tag);
+    const {id, name} = filterArtifact(release, release_tag);
+    console.log(`Artifact Id: ${id},  Artifact Name: ${name}`);
 
     const artifact_stream = await getReleaseAsset(octokit, context, id);
     console.log('Release asset downloaded.');
 
-    const buffer = toBuffer(artifact_stream);
+    const artifact_buffer = toBuffer(artifact_stream);
     console.log('ArrayBuffer converted to Buffer.');
 
-    await uploadToCloudHub(buffer, name);    
-    console.log("Action executed successfully.");
+    uploadToCloudHub(CLIENT_ID, CLIENT_SECRET, ORG_ID, artifact_buffer, name, cloudhub_apps);
 
     return true;
   }
@@ -40,71 +39,76 @@ async function main() {
 
 main();
 
-function filterArtifact(release) {
-
+function filterArtifact(release, release_tag) {
   const artifact = release.assets
                     .filter(asset => 
                       asset.name
-                        .includes(deployArgs.release_tag)
-                    );
-  
+                        .includes(release_tag));
   if (!artifact[0]) {
     throw new Error("Release artifact not found");
   }
   return artifact[0];
 }
 
-async function getRelease(octokit, context) {
-
-  if (!deployArgs.release_tag) {
-    throw new Error("Release tag name not provided.");
-  }
-
-  return (await octokit.repos.getReleaseByTag({
+async function getRelease(octokit, context, release_tag) {
+  const release = await octokit.repos.getReleaseByTag({
     ...context.repo,
-    tag: deployArgs.release_tag
-  })).data;
+    tag: release_tag
+  });
+  return release.data;
 }
 
 async function getReleaseAsset(octokit, context, assetId) {
-  let result = null;
-
-  result = (await octokit.request("GET /repos/{owner}/{repo}/releases/assets/{asset_id}", {
+  const response = await octokit.request("GET /repos/{owner}/{repo}/releases/assets/{asset_id}", {
     headers: {
       Accept: "application/octet-stream",
     },
     ...context.repo,
     asset_id: assetId
-  }));
-  return result.data;
+  });
+  return response.data;
 }
 
-async function uploadToCloudHub(artifact, artifact_name) {   
-  const { client_id, client_secret } = deployArgs.cloudhub_creds;
+function uploadToCloudHub(CLIENT_ID, CLIENT_SECRET, ORG_ID, artifact, artifact_name, cloudhub_apps) {   
+  const environments = getEnvByOrgId(CLIENT_ID, CLIENT_SECRET, ORG_ID);
 
-  for (const app of deployArgs.cloudhub_apps) {   
-
+  for (const app of cloudhub_apps) {   
+    const env = environments.filter(env => env.name.toUpperCase() == app.name.toUpperCase());
     var form_data = new FormData();
     form_data.append('file', artifact, artifact_name);
-
-    await axios({
+    axios({
       method: "post",
       url: `https://anypoint.mulesoft.com/cloudhub/api/v2/applications/${app.name}/files`,
-      auth: { client_id: client_id,  client_secret: client_secret },
+      auth: { client_id: CLIENT_ID,  client_secret: CLIENT_SECRET },
       data: form_data,
       maxContentLength: Infinity,
       maxBodyLength: Infinity,
       headers: { 
         ...form_data.getHeaders(),
         "Content-Length": form_data.getLengthSync(), 
-        'X-ANYPNT-ENV-ID': app.env_id }
+        'X-ANYPNT-ENV-ID': env[0].id }
     })
-    .then(() => {
+    .then( () => {
       console.log(app.env + " updated successfully.");
     }, (error) => {
       console.error(error);
       core.setFailed(error.message);
     })
+  }
+}
+
+async function getEnvByOrgId(CLIENT_ID, CLIENT_SECRET, ORG_ID) {
+  try {
+    const response = await axios({
+      method: "get",
+      url: `https://anypoint.mulesoft.com/accounts/api/organizations/${ORG_ID}/environments`,
+      auth: { username: CLIENT_ID,  password: CLIENT_SECRET }
+    })
+    return response.data.data;
+  }
+  catch(error) {
+    console.error(error);
+    core.setFailed(error.message);
   }
 }
 
